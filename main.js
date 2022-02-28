6d3c42f650cf05237140e92c3b42ba5198300b1c
cmoehler <ccm@gmx.net>
2022-02-28 17:04:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -227,13 +227,14 @@ if (require.main !== module) {
 function pollData(){
 	myAdapter.log.info("trigger erhalten");
 
-	let Wasserw_C3_A4chter_IP = "192.168.70.26";
-	let Wasserw_C3_A4chter_Port = "5333";
-
 	// Spannung Stützbatterie
+	GetDeviceState("BAT");
+}
+
+function GetDeviceState(Command){
 	try {
-		require("request")("http://192.168.70.26:5333/safe-tec/get/BAT", function (error, response, result) {
-			myAdapter.log.info(result);
+		require("request")("http://" + myAdapter.deviceIP + ":" + myAdapter.devicePort + "/safe-tec/get/" + Command, function (error, response, result) {
+			myAdapter.log.info("Command: " + Command + " = " + result);
 			// setState("a_andreas.0.sys_variablen.Objekt_JSON", result, true);
 		}).on("error", function (e) {myAdapter.log.error(e);});
 	} catch (e) { myAdapter.log.error(e); }
