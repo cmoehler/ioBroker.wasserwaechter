5553cbd23081a9c29734c1c179822b891e0feb42
cmoehler <ccm@gmx.net>
2022-02-28 11:46:44 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"wasserwaechter adapter settings": {
+		"en": "Adapter settings for wasserwaechter",
+		"de": "Adaptereinstellungen für wasserwaechter",
+		"ru": "Настройки адаптера для wasserwaechter",
+		"pt": "Configurações do adaptador para wasserwaechter",
+		"nl": "Adapterinstellingen voor wasserwaechter",
+		"fr": "Paramètres d'adaptateur pour wasserwaechter",
+		"it": "Impostazioni dell'adattatore per wasserwaechter",
+		"es": "Ajustes del adaptador para wasserwaechter",
+		"pl": "Ustawienia adaptera dla wasserwaechter",
+		"zh-cn": "wasserwaechter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
